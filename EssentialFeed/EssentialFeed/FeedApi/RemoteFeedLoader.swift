9c7996a26229853fef6cf36474764c78a93a2859
Tiago Amorim <tiago.amorim@wit-software.com>
2022-08-20 18:08:43 +0100

    Add memory leak detection

--- EssentialFeed/EssentialFeed/FeedApi/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/FeedApi/RemoteFeedLoader.swift
@@ -27,17 +27,22 @@ public final class RemoteFeedLoader {
     }
     
     public func load(completion: @escaping (Result) -> Void) {
-        client.get(from: url) {result in
+        client.get(from: url) { result in
             switch result {
             case let .success(data, response):
-                if let items = try? FeedItemsMapper.map(data, response) {
-                    completion(.success(items))
-                } else {
-                    completion(.failure(.invalidData))
-                }
+                completion(self?.map(data, from: response))
             case .failure:
                 completion(.failure(.connectivity))
             }
+        }
+    }
+    
+    private func map(_ data: Data, from response: HTTPURLResponse) -> Result {
+        do {
+            let items = try FeedItemsMapper.map(data, response)
+            return .success(items)
+        } catch {
+            return .failure(.invalidData)
         }
     }
 }
