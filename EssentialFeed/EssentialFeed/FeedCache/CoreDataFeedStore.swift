6c034092345138f9d91566837fb1823d1b9b6a46
Tiago Amorim <tiago.amorim@wit-software.com>
2022-09-21 08:37:44 +0100

    Update CoreDataFeedStoreTests.swift

--- EssentialFeed/EssentialFeed/FeedCache/CoreDataFeedStore.swift
+++ EssentialFeed/EssentialFeed/FeedCache/CoreDataFeedStore.swift
@@ -11,8 +11,8 @@ public final class CoreDataFeedStore: FeedStore {
     private let container: NSPersistentContainer
     private let context: NSManagedObjectContext
     
-    public init(bundle: Bundle = .main) throws {
-        container = try NSPersistentContainer.load(modelName: "FeedStore", in: bundle)
+    public init(storeURL: URL, bundle: Bundle = .main) throws {
+        container = try NSPersistentContainer.load(modelName: "FeedStore", url: storeURL, in: bundle)
         context = container.newBackgroundContext()
     }
     
@@ -30,31 +30,33 @@ public final class CoreDataFeedStore: FeedStore {
 }
 
 private extension NSPersistentContainer {
-     enum LoadingError: Swift.Error {
-         case modelNotFound
-         case failedToLoadPersistentStores(Swift.Error)
-     }
+    enum LoadingError: Swift.Error {
+        case modelNotFound
+        case failedToLoadPersistentStores(Swift.Error)
+    }
+    
+    static func load(modelName name: String, storeURL: URL, in bundle: Bundle) throws -> NSPersistentContainer {
+        guard let model = NSManagedObjectModel.with(name: name, in: bundle) else {
+            throw LoadingError.modelNotFound
+        }
+        
+        let descriptor = NSPersistentStoreDescription(url: storeURL)
+        let container = NSPersistentContainer(name: name, managedObjectModel: model)
+        container.persistentStoreDescriptions = [descriptor]
+        var loadError: Swift.Error?
+        container.loadPersistentStores { loadError = $1 }
+        try loadError.map { throw LoadingError.failedToLoadPersistentStores($0) }
+        
+        return container
+    }
+}
 
-     static func load(modelName name: String, in bundle: Bundle) throws -> NSPersistentContainer {
-         guard let model = NSManagedObjectModel.with(name: name, in: bundle) else {
-             throw LoadingError.modelNotFound
-         }
-
-         let container = NSPersistentContainer(name: name, managedObjectModel: model)
-         var loadError: Swift.Error?
-         container.loadPersistentStores { loadError = $1 }
-         try loadError.map { throw LoadingError.failedToLoadPersistentStores($0) }
-
-         return container
-     }
- }
-
- private extension NSManagedObjectModel {
-     static func with(name: String, in bundle: Bundle) -> NSManagedObjectModel? {
-         return bundle
-             .url(forResource: name, withExtension: "momd")
-             .flatMap { NSManagedObjectModel(contentsOf: $0) }
-     }
+private extension NSManagedObjectModel {
+    static func with(name: String, in bundle: Bundle) -> NSManagedObjectModel? {
+        return bundle
+            .url(forResource: name, withExtension: "momd")
+            .flatMap { NSManagedObjectModel(contentsOf: $0) }
+    }
  }
 
 
--- EssentialFeed/EssentialFeedTests/FeedCache/CoreDataFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedCache/CoreDataFeedStoreTests.swift
@@ -62,7 +62,9 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     }
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> FeedStore {
-        let sut = CoreDataFeedStore()
+        let storeBundle = Bundle(for: CoreDataFeedStore.self)
+        let storeURL = URL(fileURLWithPath: "dev/null")
+        let sut = try! CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
         trackForMemoryLeaks(sut,file: file, line: line)
         
         return sut
