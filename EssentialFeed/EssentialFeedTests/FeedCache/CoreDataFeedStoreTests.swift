d386dd5c7b74ce79c1a3d888f9ba11d338db910a
Tiago Amorim <tiago.amorim@wit-software.com>
2022-09-22 08:09:34 +0100

    CoreDataFeedStore.retrieve() delivers found values on non-empty cache

--- EssentialFeed/EssentialFeedTests/FeedCache/CoreDataFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedCache/CoreDataFeedStoreTests.swift
@@ -22,7 +22,9 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     }
     
     func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
+        let sut = makeSUT()
         
+        assertThatRetrieveDeliversFoundValuesOnNonEmptyCache(on: sut)
     }
     
     func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
@@ -63,7 +65,7 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> FeedStore {
         let storeBundle = Bundle(for: CoreDataFeedStore.self)
-        let storeURL = URL(fileURLWithPath: "dev/null")
+        let storeURL = URL(fileURLWithPath: "/dev/null")
         let sut = try! CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
         trackForMemoryLeaks(sut,file: file, line: line)
         
