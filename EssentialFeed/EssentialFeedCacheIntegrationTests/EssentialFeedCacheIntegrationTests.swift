edd2fc98bfa637bbfe08cca6e4a7ab4feeb380db
Tiago Amorim <tiago.amorim@wit-software.com>
2022-09-27 07:45:53 +0100

    Add EssencialFeedCacheIntegrationTests to separate the potentially slower cache integration tests from the unit/isolated tests

---
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,13 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by tamorim on 27/09/2022.
+//
+
+import XCTest
+
+class EssentialFeedCacheIntegrationTests: XCTestCase {
+
+    
+}
