3e4d15854903015ab18999820c8b2994b5855ec0
Tiago Amorim <tiago.amorim@wit-software.com>
2022-10-20 09:10:56 +0100

    Display image with animation

---
+++ EssentialFeed/EssentialFeediOS/Views/UIImageView+Animations.swift
@@ -0,0 +1,21 @@
+//
+//  UIImageView+Animations.swift
+//  EssentialFeediOS
+//
+//  Created by tamorim on 20/10/2022.
+//
+
+import UIKit
+
+extension UIImageView {
+    func setImageAnimated(_ newImage: UIImage?) {
+        image = newImage
+        
+        guard newImage != nil else { return }
+        
+        alpha = 0
+        UIView.animate(withDuration: 0.25) {
+            self.alpha = 1
+        }
+    }
+}
