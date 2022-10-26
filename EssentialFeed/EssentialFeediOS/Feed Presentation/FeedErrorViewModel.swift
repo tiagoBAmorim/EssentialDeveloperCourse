9d73fe790e81d5a99c913b5a692765a74bb0a1fe
Tiago Amorim <tiago.amorim@wit-software.com>
2022-10-26 09:00:53 +0100

    Move FeedErrorViewModel to separate file

---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by tamorim on 26/10/2022.
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+    let message: String?
+}
