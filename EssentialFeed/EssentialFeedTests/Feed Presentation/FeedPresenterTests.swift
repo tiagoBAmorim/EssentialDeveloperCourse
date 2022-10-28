1d3f7a0befe7499fa96a9e5231c56e51f81f76a1
Tiago Amorim <tiago.amorim@wit-software.com>
2022-10-28 08:30:41 +0100

    FeedPresenter displays feed and stops loading on didFinishLoadingFeed

--- EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -6,6 +6,11 @@
 //
 
 import XCTest
+import EssentialFeed
+
+protocol FeedView {
+    func display(_ viewModel: FeedViewModel)
+}
 
 protocol FeedLoadingView {
     func display(_ viewModel: FeedLoadingViewModel)
@@ -23,15 +28,21 @@ struct FeedErrorViewModel {
     }
 }
 
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
+
 struct FeedLoadingViewModel {
     let isLoading: Bool
 }
 
 final class FeedPresenter {
+    private let feedView: FeedView
     private let loadingView: FeedLoadingView
     private let errorView: FeedErrorView
     
-     init(loadingView: FeedLoadingView, errorView: FeedErrorView) {
+     init(feedView: FeedView, loadingView: FeedLoadingView, errorView: FeedErrorView) {
+         self.feedView = feedView
          self.loadingView = loadingView
          self.errorView = errorView
      }
@@ -39,6 +50,11 @@ final class FeedPresenter {
     func didStartLoadingFeed() {
         errorView.display(.noError)
         loadingView.display(FeedLoadingViewModel(isLoading: true))
+    }
+    
+    func didFinishLoadingFeed(with feed: [FeedImage]) {
+        feedView.display(FeedViewModel(feed: feed))
+        loadingView.display(FeedLoadingViewModel(isLoading: false))
     }
  }
 
@@ -57,25 +73,39 @@ final class FeedPresenter {
          XCTAssertEqual(view.messages, [.display(errorMessage: .none),
                                         .display(isLoading: true)])
      }
+     
+     func test_didFinishLoadingFeed_displaysFeedAndStopsLoading() {
+         let (sut, view) = makeSUT()
+         let feed = uniqueImageFeed().models
+         
+         sut.didFinishLoadingFeed(with: feed)
+         XCTAssertEqual(view.messages, [.display(feed: feed),
+                                        .display(isLoading: false)])
+     }
 
      
      // MARK: - Helpers
      
      private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedPresenter, view: ViewSpy) {
          let view = ViewSpy()
-         let sut = FeedPresenter(loadingView: view, errorView: view)
+         let sut = FeedPresenter(feedView: view, loadingView: view, errorView: view)
          trackForMemoryLeaks(view, file: file, line: line)
          trackForMemoryLeaks(sut, file: file, line: line)
          return (sut, view)
      }
      
-     private class ViewSpy: FeedLoadingView, FeedErrorView {
+     private class ViewSpy: FeedView, FeedLoadingView, FeedErrorView {
          enum Message: Hashable {
              case display(errorMessage: String?)
              case display(isLoading: Bool)
+             case display(feed: [FeedImage])
          }
          
          private(set) var messages = Set<Message>()
+         
+         func display(_ viewModel: FeedViewModel) {
+             messages.insert(.display(feed: viewModel.feed))
+         }
          
          func display(_ viewModel: FeedLoadingViewModel) {
              messages.insert(.display(isLoading: viewModel.isLoading))
