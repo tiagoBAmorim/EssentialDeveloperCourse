6c887d248661e443259e09a99ef30d3d2686afb8
Tiago Amorim <tiago.amorim@wit-software.com>
2022-10-28 08:51:40 +0100

    Move FeedViewModel to its own file

--- EssentialFeed/EssentialFeed/Feed Presentation/FeedPresenter.swift
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedPresenter.swift
@@ -31,10 +31,6 @@ public struct FeedErrorViewModel {
     }
 }
 
-public struct FeedViewModel {
-    public let feed: [FeedImage]
-}
-
 public struct FeedLoadingViewModel {
    public let isLoading: Bool
 }
---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by tamorim on 28/10/2022.
+//
+
+import Foundation
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
