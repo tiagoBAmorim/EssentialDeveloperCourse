00ece441fde091276613f0693584adbe143ba8f1
Tiago Amorim <tiago.amorim@wit-software.com>
2022-11-11 07:49:09 +0000

    Displays remote feed on launch when customer has connectivity

--- EssentialFeed/EssentialFeedTests/FeedApi/LoadFeedFromRemoteUseCasesTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedApi/LoadFeedFromRemoteUseCasesTests.swift
@@ -114,8 +114,8 @@ class LoadFeedFromRemoteUseCasesTests: XCTestCase {
         let client = HTTPClientSpy()
         let sut = RemoteFeedLoader(url: url, client: client)
         
-        trackForMemoryLeaks(client)
-        trackForMemoryLeaks(sut)
+        trackForMemoryLeaks(client, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
         
         return (sut, client)
     }
